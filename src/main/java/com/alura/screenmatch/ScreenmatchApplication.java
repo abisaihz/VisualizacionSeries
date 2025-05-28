5eafd5b13a54a34b39dd8410120154544a28e119
Abisai Hernandez <ajhernande@outlook.com>
2025-05-28 09:33:44 -0700

    Inicializando respaldo

---
+++ src/main/java/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,31 @@
+package com.alura.screenmatch;
+
+import com.alura.screenmatch.model.DatosSerie;
+import com.alura.screenmatch.service.ConsumoAPI;
+import com.alura.screenmatch.service.ConvierteDatos;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obtenerDatos("https://omdbapi.com/?t=game+of+thrones&apikey=acba02d5");
+		System.out.println(json);
+		ConvierteDatos conversor = new ConvierteDatos();
+		var datos = conversor.obtenerDatos(json, DatosSerie.class);
+		System.out.println(datos);
+
+
+
+	}
+
+
+}
